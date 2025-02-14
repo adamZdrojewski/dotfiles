7d47e6b77d5c48b8f6c07e6b13c8032a5e309b71
adamZdrojewski <adam.zdrojewski2@gmail.com>
2025-02-14 18:17:27 -0500

    Updates cp-button-website tmuxifier script

--- tmux-layouts/.tmux-layouts/cp-button-website.session.sh
+++ tmux-layouts/.tmux-layouts/cp-button-website.session.sh
@@ -6,17 +6,19 @@ session_root "~/Projects/cp-button-website"
 # argument is given, session name will be based on layout file name.
 if initialize_session "cp-button-website"; then
 	# Editor window
-	new_window "editor"
+	new_window "Editor"
 	run_cmd nvim
 
 	# Web server window
-	new_window "server"
+	new_window "Server"
 	tmux split-window -t "$session:$window.0" -h -p 50
 	select_pane 0
 	run_cmd "npm run pages:dev"
+	select_pane 1
+	run_cmd "cd ~/Projects/cp-button-website"
 
 	# Lazygit window
-	new_window "git"
+	new_window "Git"
 	run_cmd lazygit
 fi
 
