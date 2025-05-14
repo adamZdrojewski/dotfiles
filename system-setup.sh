13005470f9dc048d23d941ddbfbe691e7ddd4064
adamZdrojewski <adam.zdrojewski2@gmail.com>
2025-05-13 23:33:21 -0400

    Adds basic draft of system setup script

---
+++ system-setup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# System Setup
+
+# Install yay
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm base-devel git
+cd ~/Downloads
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+rm -rf ~/Downloads/yay
+
+# Install GNU stow & create symlinks for dotfiles
+sudo pacman -S --noconfirm stow
+cd ~
+git clone https://github.com/adamZdrojewski/dotfiles.git
+
+rm .bashrc
+cd ~/dotfiles
+stow bashrc
+stow bin
+stow ghostty
+stow hypr
+stow lazygit
+stow nvim
+stow rofi
+stow starship
+stow tmux
+stow waybar
+
+cd ~
+
+# Install terminal packages
+terminal_packages=(
+	ghostty
+	neovim
+	ripgrep
+	xclip
+	tmux
+	starship
+	lazygit
+	man-db
+	ttf-hack-nerd
+)
+
+for package in ${terminal_packages[@]}; do
+	sudo yay -S --noconfirm ${package}
+done
+
+# Install tmux plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+# Install Hyprland
+hyprland_packages=(
+	hyprland
+	waybar
+	swaync
+	hyprlock
+	hypridle
+	hyprpaper
+	hyprpolkitagent
+	wlsunset
+	rofi
+	hyprshot
+	catppuccin-cursors-mocha
+	adw-gtk-theme-git
+)
+
+for package in ${hyprland_yay_packages[@]}; do
+	sudo yay -S --noconfirm ${package}
+done
+
+# Install other misc packages
+misc_packages=(
+	google-chrome
+	spotify-launcher
+)
+
+for package in ${hyprland_yay_packages[@]}; do
+	sudo yay -S --noconfirm ${package}
+done
