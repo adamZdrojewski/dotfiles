f66cb4aac6bd889a4882669da9cd503dffe9d900
adamZdrojewski <adam.zdrojewski2@gmail.com>
2025-05-28 20:47:27 -0400

    Adds keybind for opening vim diagnostic float

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -58,10 +58,11 @@ return {
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+			--[[vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})]]--
 		end,
 	},
 }
